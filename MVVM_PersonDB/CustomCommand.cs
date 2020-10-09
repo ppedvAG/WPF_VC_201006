8809097ae926922c83512f9f25cb69746cb3487c
KlaasHagge <klaash@ppedv.de>
2020-10-09 11:22:24 +0200

    MVVM-Start

---
+++ MVVM_PersonDB/CustomCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MVVM_PersonDB
+{
+    public class CustomCommand : ICommand
+    {
+        public CustomCommand(Func<object, bool> can, Action<object> exe)
+        {
+            CanExecuteMethode = can;
+            ExecuteMethode = exe;
+        }
+
+        public Action<object> ExecuteMethode { get; set; }
+        public Func<object, bool> CanExecuteMethode { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteMethode(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteMethode(parameter);
+        }
+    }
+}
---
+++ MVVM_PersonDB/ViewModel/StartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using MVVM_PersonDB.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVM_PersonDB.ViewModel
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public int AnzahlPerson { get { return Model.Person.Personenliste.Count; } }
+
+        public CustomCommand LadeDbCmd { get; set; }
+
+        public CustomCommand OeffneDbCmd { get; set; }
+
+        public StartViewModel()
+        {
+            LadeDbCmd = new CustomCommand
+                (
+                    p => this.AnzahlPerson == 0,
+                    p =>
+                    {
+                        Model.Person.LadePersonenAusDb();
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnzahlPerson)));
+                    }
+                );
+
+            OeffneDbCmd = new CustomCommand
+                (
+                    p => this.AnzahlPerson > 0,
+                    p =>
+                    {
+                        View.ListView db_Ansicht = new View.ListView();
+
+                        db_Ansicht.Show();
+
+                        (p as StartView).Close();
+                    }
+                );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
